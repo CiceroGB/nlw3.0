c1e1fb469e07942dd7451cbda517125fca1f704e
Cicero Gasparini Brunelli <brunelli.cg@gmail.com>
2021-03-16 22:41:26 -0300

    controllers images table

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,44 +1,18 @@
 import express from 'express';
 
-import { getRepository } from 'typeorm';
-import Orphanages from './models/Orphanage';
 
 import './database/connection';
+import routes from './routes';
 
 
 const app = express();
 
+
+
 app.use(express.json());
+app.use(routes);
 
- app.post('/orphanages', async (req, res) => {
-    const {
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends,
-    } = req.body;
-
-    const orphanagesRepository = getRepository(Orphanages);
-
-    const orphanages = orphanagesRepository.create({
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends,
-    })
-
-    await orphanagesRepository.save(orphanages);
-
-    return res.json({ msg: 'HI' })
-})
-
-
-
-app.listen(3333);
+app.listen(3333, () => {
+    console.log('Server started!');
+  });
 
